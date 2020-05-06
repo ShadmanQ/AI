b56c31288baa4072ff869635b890d09ccc77a4cd
ShadmanQ <shadmanhquazi@gmail.com>
2020-05-06 19:02:09 -0400

    updates to things

--- Homework 3/starter_code/problem1.py
+++ Homework 3/starter_code/problem1.py
@@ -1,11 +1,28 @@
 import numpy as np
 import sys
+import csv
 
 
-hi = sys.argv
+class Perceptron():
+    training_data = None
+    weights = None
 
-print(hi)
+    def __init__(self, training_data, weights):
+        self.training_data = training_data
+        self.weights = weights
+
+    def predict(self):
+        for line in self.training_data:
+            print(line)
 
 
+def main():
+    input_args = sys.argv
+    data = np.loadtxt(input_args[1], delimiter=',', dtype=float)
+    weights = np.zeros(3)
+    Pete = Perceptron(data, weights)
+    print(Pete.training_data)
+    Pete.predict()
 
-weights []
+
+main()
