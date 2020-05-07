0f861eb50b3b610a7074a6a85354c4fae8470e99
ShadmanQ <shadmanhquazi@gmail.com>
2020-05-07 15:22:30 -0400

    updated Perceptron class

--- Homework 3/starter_code/problem1.py
+++ Homework 3/starter_code/problem1.py
@@ -10,19 +10,24 @@ class Perceptron():
     def __init__(self, training_data, weights):
         self.training_data = training_data
         self.weights = weights
+        self.bias = np.ones(self.training_data.shape[0])
 
-    def predict(self):
-        for line in self.training_data:
-            print(line)
+    def train(self):
+        for i in range(len(self.training_data)):
+            eval = self.bias[i] * self.weights[0] + self.training_data[i][0] * \
+                self.weights[1] + self.training_data[i][1] + \
+                self.weights[2] * self.training_data[i][2]
+            print(eval, np.sign(eval) == 1)
 
 
 def main():
     input_args = sys.argv
     data = np.loadtxt(input_args[1], delimiter=',', dtype=float)
+    print(data.shape)
     weights = np.zeros(3)
     Pete = Perceptron(data, weights)
-    print(Pete.training_data)
-    Pete.predict()
+    # print(Pete.training_data)
+    Pete.train()
 
 
 main()
